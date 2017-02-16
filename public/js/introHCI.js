6f77582850dbb8e9e9a98323abf69271f4c345b6
Tina Ke <tjke@ucsd.edu>
2017-02-16 12:49:30 -0800

    submitting lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("project/"+idNumber, addProject);
+	console.log("project/"+idNumber);
+}
+
+// the callback function
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p>'+result['summary'];
+
+	//$("#project-container").html(projectHTML);
+	//$("#project-description").html(result['summary']);
+	$("#project"+result['id']+" .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +47,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+}
+
+// callback
+function addColor(result) {
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
